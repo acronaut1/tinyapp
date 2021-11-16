a2664f8858de7ef1f2b1e2e69523ab7a0bd8c74b
acronaut1 <opruski@ryerson.ca>
2021-11-15 23:57:12 -0500

    CLEAN verbosity

--- express_server.js
+++ express_server.js
@@ -10,15 +10,8 @@ const helpers = require('./helpers.js');
 function generateRandomString() {
   // Generate [Random AlphaNumeric String]..
   let rndStr = Math.random(2).toString(36).slice(2, 8);
-  //console.log(rndStr);
   return rndStr;
 }
-
-// // set user_id key on a session...
-// const id = req.session.user_id = "some value";
-// // read a value..
-// req.session.user_id;
-
 
 const users = {
   userRandomID: {
@@ -45,28 +38,30 @@ const urlDatabase = {
   }
 };
 
-
+// [Mogan]: Middleware Logger..
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cookieParser());
 
 app.use(
   cookieSession({
     name: 'session',
     keys: ['email', 'password'],
 
-    // Cookie options
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    // 24 hours
+    maxAge: 24 * 60 * 60 * 1000, 
   })
 );
-
 
 app.set("view engine", "ejs");
 
 // **LAND**[ROOT]
 app.get("/", (req, res) => {
   const randURL = generateRandomString();
-  res.send(`<html><body>Welcome to <b>TinyApp</b><br>I take Long URLS, and I shrink them like this: ${randURL}<br><br><a href="/urls"> Come on in!</a></body></html>\n`);
+  res.send(`<html><body>Welcome to 
+  <b>TinyApp</b><br>I take Long URLS, 
+  and I shrink them like this: ${randURL}
+  <br><br><a href="/urls"> Come on in!
+  </a></body></html>\n`);
 });
 
 // **CREATE**[URLS: Create New URL]
@@ -76,14 +71,20 @@ app.get("/urls/new", (req, res) => {
   if (!user) {
     return res.redirect("/login");
   }
-  const templateVars = { user: users[req.session.user_id] };  
+  const templateVars = { 
+    user: users[req.session.user_id]
+  };  
   res.render("urls_new", templateVars);
 });
 
 // **VIEW**[URLS: Display shortenURL]
 app.get("/urls/:shortURL", (req, res) => {
   const userEdit = req.session.user_id;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL , user: users[req.session.user_id] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL].longURL,
+    user: users[req.session.user_id]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -91,37 +92,14 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const url = urlDatabase[req.params.id];
   if (!url) {
-    return res.status(401).send("ERROR..");
+    return res.status(401).send("This Shorted Link has not been claimed...yet!<br><a href='/register'>Claim One Today</a>..");
   }
   const longURL = url.longURL;
   console.log(`longURL:`, longURL);
   res.redirect(longURL);
 });
 
-// // **VISIT**[URLS: Link to a ShortenURL]
-// app.get("/u/:shortURL", (req, res) => {
-//   const longURL = urlDatabase[req.params.shortURL].longURL;
-//   console.log(`longURL:`, longURL);
-//   res.redirect(longURL);
-// });
-
-
-// [DEBUG][URLS: Urls JSON]
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// [DEBUG][URLS: Urls JSON]
-app.get("/users.json", (req, res) => {
-  res.json(users);
-});
-
-// [DEBUG][URLS: Urls JSON]
-app.get("//usr2.email", (req, res) => {
-  res.json(users.user2RandomID.email);
-});
-
-
+// Input Validator
 const validInputCheck = (email, password) => {
   if (email === '' || password === '') {
     return false;
@@ -129,6 +107,7 @@ const validInputCheck = (email, password) => {
   return true;
 };
 
+// Retrieves Registerd User's Shorted Links..
 const getUserUrls = (user_id) => {
   const urls = {};
   console.log(`urlDB:`, urlDatabase);
@@ -141,43 +120,6 @@ const getUserUrls = (user_id) => {
   return urls;
 }
 
-// const getUserUrls = (user_id, urlDB) => {  // TO DO: external reference
-//   const urls = {};
-//   console.log(`urlDB:`, urlDB);
-//   for (let i in urlDB) {
-//     let url = urlDB[i];
-//     if (url.userID === user_id) {
-//       urls[i] = url; 
-//     }
-//   }
-//   return urls;
-// }
-
-
-
-// const getUserByEmail = (email, database) => {
-//   console.log(`GUBE_users-obj:`, database);
-//   console.log(`pre-forloopEmail:`, email);
-//   for (const user in database) {
-//     if (email === database[user].email) {
-//       return database[user];
-//     }
-//   }
-//   return null;
-// };
-
-
-// const getUserByEmail = (email, database) => {
-//   console.log(`GUBE_users-obj:`, users);
-//   console.log(`pre-forloopEmail:`, email);
-//   for (const user in users) {
-//     if (email === users[user].email) {
-//       return users[user];
-//     }
-//   }
-//   return null;
-// };
-
 const passwordCheck = (user, password) => {
   if (user === null) {
     return null;
@@ -185,8 +127,7 @@ const passwordCheck = (user, password) => {
   return bcrypt.compareSync(password, user.password);
 };
 
-
-// **VIEW** [URLS: Main..]
+// **VIEW** [If Registered ? List of Links : Login Page]
 app.get("/urls", (req, res) => {
   console.log(`reg.session:`, req.session);
   const id = req.session.user_id;
@@ -201,69 +142,53 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-
-// bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//   // Store hash in your password DB.
-// });
-
-
-
-
-// login page****
+// **LOGIN ACCOUNT**
 app.get('/login', (req, res) => {
   const id = req.session.user_id;
   const user = users[id];
   if (user) {
     return res.redirect("/urls");
   }
-  
   res.render("usr_login", { user });
-  //res.render('usr_login', templateVars);
 });
 
-// **REGISTER** ****
+// **REGISTER ACCOUNT** 
 app.get("/register", (req, res) => {
   const id = req.session.user_id;
   const user = users[id];
   if (user) {
     return res.redirect("/urls");
   }
-
   res.render("usr_register", { user });
-  //res.render("usr_register", templateVars);
 });
 
 
-// //The Blackhole-redirect; Returns the index page when no other route is matched.
-// app.get("*", (req, res) => {
-//   console.log("Wanderer sent-back to the Index..")
-//   res.redirect("/urls");
-//   //generateRandomString();
-// });
+//The Blackhole-redirect; Returns Curious-User towards Register Page.. 
+app.get("*", (req, res) => {
+  console.log("Wanderer sent-back to the Index..")
+  res.redirect("/register");
+});
 
-
-// **DELETE** ShortenURL Link
+// **DELETE** Shorted URL Link..
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userID = req.session.user_id;
   const user = users[userID];
   if (!user) {
     return res.redirect('/login');
   }
-  console.log('a Link have been Deleted!', req.params.shortURL);
+  console.log('The Link have been Deleted!', req.params.shortURL);
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
  
 
-// ****
+// **CREATE** Shorted URL -> Return to User's List..
 app.post("/urls/:shortURL", (req, res) => {
-  // ****
   const userID = req.session.user_id;
   const user = users[userID];
   if (!user) {
     return res.redirect('/urls');
   }
-  // ****
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = { userID, longURL };
@@ -284,7 +209,7 @@ app.post('/urls', (req, res) => {
   urlDatabase[shortURL] = { 
     longURL : longURL,
     userID : req.session.user_id
-    };
+  };
   console.log('User:', req.session.user_id, ' Shorted ', req.body.longURL, ' with ', shortURL);
   res.redirect(`/urls/${shortURL}`);
 });
@@ -293,10 +218,11 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10);
-
   const id = generateRandomString();
+  
   // (eNp = email AND password..)
   const eNp = validInputCheck(email, password);
+  
   if (!eNp) {
     return res.status(403).send("Please enter BOTH Email & Password..");
   }
@@ -304,12 +230,9 @@ app.post("/register", (req, res) => {
   if (user) {
     return res.status(403).send("Email already Exist! <a href='/login'>Try Again</a>");
   }
-  //console.log(email, password);
   users[id] = { id, email, password: hashedPassword};
-  //res.cookie("user_id", id);
-  req.session.user_id = id;   // not 100%...
+  req.session.user_id = id;   
   res.redirect("/urls");
-
 });
 
 // User_id gets Logged in..
@@ -328,12 +251,11 @@ app.post("/login", (req, res) => {
   }
   console.log(users);
   req.session.user_id = user.id
-  // res.session('user_id', user.id);
   res.redirect("/urls");
 });
 
+// **LOG-OUT** 
 app.post("/logout", (req, res) => {
-  //res.clearCookie('user_id');
   req.session = null;
   res.redirect("/urls");
 });
--- helpers.js
+++ helpers.js
@@ -1,7 +1,4 @@
-
 const getUserByEmail = (email, database) => {
-  console.log(`GUBE_users-obj:`, database);
-  console.log(`pre-forloopEmail:`, email);
   for (const user in database) {
     if (email === database[user].email) {
       return database[user];
